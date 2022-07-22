3044cc15362711cda73b0ab158e1abaf6a7929ef
nimishdetroja <nimishsdetroja56@gmail.com>
2022-07-23 00:48:14 +0530

    first commit

---
+++ ifelse.py
@@ -0,0 +1,17 @@
+
+# a = 33
+# b = 200
+# if a < b: print("a is less than b")
+
+# a = 33
+# b = 200
+# if b > a:
+#     print("b is greater than a")
+
+# if_else
+# a = 200
+# b = 330
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("a is greater than b")
